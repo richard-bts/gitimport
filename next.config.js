cfc1d648d5011054cdeff8a1a14ab1c0b2c03e7f
Broadview Tech <btech209@ad.cdldelivers.com>
2022-08-19 14:03:48 -0400

    updated config file for nested subdomain application

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  /**
+   * Add the code below if you are nesting your application under Xcelerator. 
+   * For the example below, appliction name is NextTest and nested under Xcelerator on IIS
+   */
+  /*
+  basePath: '/Xcelerator/NextTest',
+  images: {
+      path: `Xcelerator/NextTest/_next/image`,
+    },
+  async rewrites() {
+      return [
+        {
+          source: '/Xcelerator/NextTest/',
+          destination: '/Xcelerator/NextTest/index'           
+        },
+      ]
+    }*/
 }
 
 module.exports = nextConfig
